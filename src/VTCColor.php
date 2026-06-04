4b0762e15c960b955a632e10cb65086494ef721f
DarthCorvidus <github@vm01.telton.de>
2026-06-04 12:25:20 +0200

    chore: add missing copyright header

--- src/VTCColor.php
+++ src/VTCColor.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @copyright (c) 2021, Claus-Christoph Küthe
+ * @author Claus-Christoph Küthe <plibv4@vm01.telton.de>
+ * @license LGPLv2.1
+ */
 namespace plibv4\vtc;
 enum VTCColor: int {
 	case NONE = 0;
